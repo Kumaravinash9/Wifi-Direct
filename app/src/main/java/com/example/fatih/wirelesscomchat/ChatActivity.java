fd2d3dfd4372a1e22dfb4eef6e57631cfecf71d6
Avinash Kumar <bhaiavinash9@gmail.com>
2021-03-28 04:28:18 +0530

    wifi_direct

---
+++ app/src/main/java/com/example/fatih/wirelesscomchat/ChatActivity.java
@@ -0,0 +1,172 @@
+package com.example.fatih.wirelesscomchat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private ServerSocket serverSocket;
+    Handler updateConversationHandler;
+    Thread serverThread = null;
+    public static final int SERVERPORT = 6000;
+    private static String SERVER_IP;
+    private Socket socket;
+
+
+    TextView receivedText;
+    EditText yourMessage;
+    Button send;
+    boolean owner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        Intent intent = getIntent();
+
+        owner = intent.getBooleanExtra("Owner?", false);
+        SERVER_IP = intent.getStringExtra("Owner Address");
+        receivedText = (TextView) findViewById(R.id.text_incoming);
+        yourMessage = (EditText) findViewById(R.id.text_send);
+        send = (Button) findViewById(R.id.btn_send);
+
+        updateConversationHandler = new Handler();
+
+        // If we're the owner start a server, else connect to the server
+        if (owner) {
+            this.serverThread = new Thread(new ServerThread());
+            this.serverThread.start();
+        } else {
+            new Thread(new ClientThread()).start();
+        }
+
+        // Sends the text to the server
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String str = yourMessage.getText().toString();
+                    PrintWriter out = new PrintWriter(new BufferedWriter(
+                            new OutputStreamWriter(socket.getOutputStream()
+                            )), true);
+                    out.println(str);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Opens a socket on the owner device to get messages
+    class ServerThread implements Runnable {
+        @Override
+        public void run() {
+            Socket socket = null;
+            try {
+                // Create a socket on port 6000
+                serverSocket = new ServerSocket(SERVERPORT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    // Start listening for messages
+                    socket = serverSocket.accept();
+                    CommunicationThread commThread = new CommunicationThread(socket);
+                    new Thread(commThread).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // Handles received messages from clients
+    class CommunicationThread implements Runnable {
+        private Socket clientSocket;
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            try {
+                // read received data
+                this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String read = input.readLine();
+                    updateConversationHandler.post(new UpdateUIThread(read));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    // Handles showing received messages on screen
+    class UpdateUIThread implements Runnable {
+        private String msg;
+
+        public UpdateUIThread(String str) {
+            this.msg = str;
+        }
+
+        // Print message on screen
+        @Override
+        public void run() {
+            receivedText.setText(receivedText.getText().toString() + " Gelen Mesaj: " + msg + "\n");
+        }
+    }
+
+    // Handles connection to server
+    class ClientThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                InetAddress serverAddress = InetAddress.getByName(SERVER_IP);
+                socket = new Socket(serverAddress, SERVERPORT);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fatih/wirelesscomchat/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.fatih.wirelesscomchat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements WifiP2pManager.PeerListListener,
+        WifiP2pManager.ConnectionInfoListener{
+
+    private final String TAG = this.getClass().toString();
+
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+
+    ArrayList<WifiP2pDevice> deviceList;
+    ArrayList deviceNames;
+    ListView deviceListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        deviceNames = new ArrayList();
+        deviceListView = (ListView) findViewById(R.id.device_list);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        Button btn_discover = (Button) findViewById(R.id.btn_discover);
+
+        // Check if we can scan for devices
+        btn_discover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {    
+                    // Yep, we can scan for devices
+                    @Override
+                    public void onSuccess() {
+
+                        deviceListView.setAdapter(null);
+                      Toast.makeText(getApplicationContext(), "Discovery is a success.",
+                                Toast.LENGTH_SHORT).show();
+                       //startActivity(new Intent(Setting.ACTION_WIRELESS_SETTINGS));
+                    }
+
+                    // Nope, can't scan for devices
+                    @Override
+                    public void onFailure(int reasonCode) {
+
+                        Toast.makeText(getApplicationContext(), "Discovery is a failure " + reasonCode,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        // Connect to selected device
+        deviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                connectToDevice(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+                
+    // Get device names after the scan and list them on the screen
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+        if (wifiP2pDeviceList.getDeviceList().size() > 0) {
+            deviceList = new ArrayList(wifiP2pDeviceList.getDeviceList());
+            for (WifiP2pDevice device : deviceList) {
+                deviceNames.add(device.deviceName);
+            }
+            ArrayAdapter adapter = new ArrayAdapter(this,
+                    android.R.layout.simple_list_item_1, deviceNames);
+            deviceListView.setAdapter(adapter);
+        }
+    }
+
+    // Connect to the selected device
+    void connectToDevice(int position) {
+        final WifiP2pDevice toConnect = deviceList.get(position);
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = toConnect.deviceAddress;
+        config.groupOwnerIntent = 15;
+        mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+                
+            @Override
+            public void onSuccess() {
+                Log.i(TAG, "Successfully connectted to "+toConnect.deviceName);
+            }
+
+            @Override
+            public void onFailure(int reason) {
+               Log.i(TAG, "Error while connecting to device. Reason code: "+reason);
+            }
+        });
+
+    }
+
+    @Override
+    public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
+        // move owner info from main screen to the chat screen
+        if (wifiP2pInfo.isGroupOwner == true){
+            Toast.makeText(getApplicationContext(), "Yay I am the owner!!", Toast.LENGTH_LONG).show();
+            Intent chatIntent = new Intent(MainActivity.this, ChatActivity.class);
+            chatIntent.putExtra("Owner?",true);
+            MainActivity.this.startActivity(chatIntent);
+        } else {
+            Toast.makeText(getApplicationContext(), "The owner is: "+
+                    wifiP2pInfo.groupOwnerAddress.getHostAddress(), Toast.LENGTH_LONG).show();
+            Intent chatIntent = new Intent(MainActivity.this, ChatActivity.class);
+            chatIntent.putExtra("Owner?",false);
+            chatIntent.putExtra("Owner Address", wifiP2pInfo.groupOwnerAddress.getHostAddress());
+            MainActivity.this.startActivity(chatIntent);
+        }
+    }
+}
